e3031add915dd9108b939826555765090d8c0f07
Rowick Legaspi <krystoffer.legaspi@globe.com.ph>
2025-08-04 18:39:37 +0800

    add fnm config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,11 +1,9 @@
-if status is-interactive
-   # Commands to run in interactive sessions can go here
-end
 set -g fish_greeting
 fish_config theme choose Nord
 
 starship init fish | source
 fzf --fish | source
+fnm env --use-on-cd --shell fish | source
 
 if type -q fdfind
     alias fd="fdfind"
