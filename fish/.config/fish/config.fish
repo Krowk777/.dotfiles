8e3cd467c39aaf95209ab1b7b6778f8db37f358d
krowk777 <krowk77777@gmail.com>
2025-09-08 19:45:58 +0800

    update fish config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -23,4 +23,5 @@ function fish_user_key_bindings
     fish_default_key_bindings -M insert
     fish_vi_key_bindings --no-erase
     bind -M insert \cj "fish_vi_key_bindings --no-erase default"
+    bind -M insert \cn down-or-search
 end
