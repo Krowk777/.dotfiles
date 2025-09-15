c7b48e4787cd3f45ab4bccdc54677e3eb0af5d84
krowk777 <krowk77777@gmail.com>
2025-09-15 14:11:39 +0800

    update wezterm window config

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -11,7 +11,7 @@ config.font = wezterm.font("FiraCode Nerd Font")
 config.font_size = 11
 
 config.enable_tab_bar = false
-config.window_decorations = "NONE"
+config.window_decorations = "RESIZE"
 
 config.enable_wayland = false
 
