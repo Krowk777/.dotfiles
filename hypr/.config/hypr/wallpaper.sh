5bb5c47733fb3046c43227d83be4df92425ba174
Krowk777 <krowk77777@gmail.com>
2026-04-13 09:26:43 +0800

    update hypr

--- hypr/.config/hypr/wallpaper.sh
+++ hypr/.config/hypr/wallpaper.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
-WALLPAPER_DIR="$HOME/wallpapers/"
-CURRENT_WALL=$(hyprctl hyprpaper listloaded)
+WALLPAPER_DIR="$HOME/wallpapers"
+CURRENT="$WALLPAPER_DIR/.current"
+touch "$CURRENT"
+
+CURRENT_WALLPAPER=$(cat "$CURRENT")
 FOCUSED_MONITOR=$(hyprctl monitors -j | jq -r ".[] | select(.focused) | .name")
-WALLPAPER=$(find "$WALLPAPER_DIR" -type f ! -name "$(basename "$CURRENT_WALL")" | shuf -n 1)
-hyprctl hyprpaper reload "$FOCUSED_MONITOR","$WALLPAPER"
+WALLPAPER=$(find "$WALLPAPER_DIR/" -type f ! -name "$CURRENT_WALLPAPER" ! -name ".current" | shuf -n 1)
+echo "$(basename "$WALLPAPER")" >"$CURRENT"
+
+until hyprctl hyprpaper wallpaper "$FOCUSED_MONITOR,$WALLPAPER,"; do
+	sleep 1
+done
